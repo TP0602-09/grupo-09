b665f7807039f506a4e1e61084fef10c6982b193
Maria Agustina Markosich <ma.markosich@yahoo.com.ar>
2016-09-21 20:05:42 -0300

    configuracion sources, corre el proyecto

---
+++ src/Model/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void sayHello() {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
