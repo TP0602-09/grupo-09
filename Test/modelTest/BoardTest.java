7377ecc02ae79a4fcad86077ad928f63425b5071
Lucía <luia.julia.r@gmail.com>
2016-09-29 08:54:39 -0300

    boardTest

---
+++ Test/modelTest/BoardTest.java
@@ -0,0 +1,39 @@
+package modelTest;
+
+import Model.Board;
+import Model.Cell;
+import Model.EditableCell;
+import Model.Position;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Lucía on 29/9/2016.
+ */
+public class BoardTest {
+
+    private Board board;
+    private HashMap<Position, Cell> cells;
+
+    @Before
+    public void setUp() {
+        cells = new HashMap<Position, Cell>();
+        for (int i = 1; i < 10; i ++) {
+            Cell cell = new EditableCell(new Position(i,i));
+            cells.put(new Position(i,i), cell);
+        }
+
+        board = new Board(9, 9, cells);
+    }
+
+    @Test
+    public void getBoardDimension() {
+        assertEquals(9, board.getRows());
+    }
+
+}
