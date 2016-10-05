2ae265a2f494c10d04153fcbf88fa378ea3931df
Lucía <luia.julia.r@gmail.com>
2016-10-05 01:20:49 -0300

    cambio en la libreria del json, ahora usamos gson

---
+++ src/model/GameNotFoundException.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by Lucía on 5/10/2016.
+ */
+public class GameNotFoundException extends Exception {
+
+    public GameNotFoundException(String message) {
+        super(message);
+    }
+}
