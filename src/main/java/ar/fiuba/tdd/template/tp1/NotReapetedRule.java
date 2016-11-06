70c411ef14346444c7fa3cad3dd51dc56ad04ff1
busiris <iggalvez2015@gmail.com>
2016-11-06 18:56:10 -0300

    agregada regla para columnas y filas del hitori

---
+++ src/main/java/ar/fiuba/tdd/template/tp1/NotReapetedRule.java
@@ -0,0 +1,27 @@
+package ar.fiuba.tdd.template.tp1;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Ignacio on 6/11/2016.
+ */
+public class NotReapetedRule extends Rule {
+    private final int BLACK = 0;
+
+    @Override
+    public boolean isValidSector(Sector sector) {
+        List<BoardElement> boardElements = sector.getBoardElements();
+
+        for (BoardElement boardElement:boardElements) {
+            DoubleValueCell doubleValueCell = (DoubleValueCell) boardElement;
+            if (doubleValueCell.getDoubleValue() == BLACK) {
+                boardElements.remove(boardElement);
+            }
+        }
+        Set<BoardElement> boardElementSet = new HashSet<>(boardElements);
+        return boardElements.size() == boardElementSet.size();
+
+    }
+}
